0f30e911a985f6bbaf265b3d37ff20c9086cc465
speedUpDev <70540091+speedUpDev@users.noreply.github.com>
2021-11-21 14:41:57 +0500

    filter.py added docstring documentation

--- filter.py
+++ filter.py
@@ -3,17 +3,22 @@ import numpy as np
 
 
 def get_pixel(i, j):
+    """Фунцкция высчитывает среднюю сумму всех цветов(RGB) в пикселях, которые будут входить в серый пиксель размером
+    pxSize*pxSize """
     sum = np.sum(arr[i:i + pxSize, j: j + pxSize]) / 3
     return int(sum // (pxSize * pxSize))
 
 
 def set_grey_pixels(colorSum, i, j):
+    """Функция перекрашивает RGB цвета в серый цвет, в каждом пикселе входящем в этот серый пиксель"""
     arr[i:i + pxSize, j:j + pxSize, 0] = int(colorSum // gradation) * gradation
     arr[i:i + pxSize, j:j + pxSize, 1] = int(colorSum // gradation) * gradation
     arr[i:i + pxSize, j:j + pxSize, 2] = int(colorSum // gradation) * gradation
 
 
 def img2pixel(original, result):
+    """Функция преобразует картинку в пиксельарт, на вход подается название оригинала и имя файла для сохранения
+    измененной картинки """
     global arr
     global gradation
     global pxSize
